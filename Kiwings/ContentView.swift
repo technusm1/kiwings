7a965ba6cfd12b793f616d756cd0c0617a9c6e9e
Maheep Kumar Kathuria <me@maheepk.net>
2021-06-16 10:38:52 +0530

    Initial Commit

---
+++ Kiwings/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kiwings
+//
+//  Created by Maheep Kumar Kathuria on 16/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kiwings/KiwingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KiwingsApp.swift
+//  Kiwings
+//
+//  Created by Maheep Kumar Kathuria on 16/06/21.
+//
+
+import SwiftUI
+
+@main
+struct KiwingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
